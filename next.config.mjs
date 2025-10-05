1149761bb9a2f7308e01bb5bd226b549a0496a2d
branJS <b.a@live.co.uk>
2025-10-05 14:00:10 +0100

    Fix export for verce;

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "export",          // ✅ This enables static export
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }
 };
+
 export default nextConfig;
